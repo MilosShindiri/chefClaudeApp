9130c03bbb625bb89298138ab580169ef138ea86
Milos_Petrovic <milos.petrovic@shindiristudio.com>
2025-08-11 13:37:00 +0200

    making of header

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <img
+        src="/images/chef-claude-icon.png"
+        alt="earth icon"
+        className="headerImg"
+      />
+      <span>Chef Claude</span>
+    </header>
+  );
+}
